88955b8e6ef85b35e9a114b5744796b8800a6c44
jernkun <jernjern356@gmail.com>
2022-09-19 17:39:09 -0400

    Q3 Done

--- Q3/contracts/bonus/SystemOfEquations.circom
+++ Q3/contracts/bonus/SystemOfEquations.circom
@@ -1,7 +1,7 @@
 pragma circom 2.0.0;
 
 include "../../node_modules/circomlib/circuits/comparators.circom";
-include ""; // hint: you can use more than one templates in circomlib-matrix to help you
+include "../../node_modules/circomlib-matrix/circuits/matMul.circom"; // hint: you can use more than one templates in circomlib-matrix to help you
 
 template SystemOfEquations(n) { // n is the number of variables in the system of equations
     signal input x[n]; // this is the solution to the system of equations
@@ -10,6 +10,32 @@ template SystemOfEquations(n) { // n is the number of variables in the system of
     signal output out; // 1 for correct solution, 0 for incorrect solution
 
     // [bonus] insert your code here
+    component mul = matMul(n,n,1);
+    for (var i=0;i<n;i++) {
+        mul.b[i][0] <== x[i];
+	for (var j=0;j<n;j++) {
+            mul.a[i][j] <== A[i][j];
+        } 
+    } 
+
+    component isEqual[n];
+    signal mem[n];
+
+    isEqual[0] = IsEqual();
+    isEqual[0].in[0] <== mul.out[0][0];
+    isEqual[0].in[1] <== b[0];
+    mem[0] <== isEqual[0].out;
+
+    for (var i=1; i<n; i++) {
+        isEqual[i] = IsEqual();
+        isEqual[i].in[0] <== mul.out[i][0];
+        isEqual[i].in[1] <== b[i];
+        mem[i] <== mem[i-1] * isEqual[i].out;
+    }
+    out <== mem[n-1];
+
+
+
 }
 
 component main {public [A, b]} = SystemOfEquations(3);
---
+++ Q3/test/test.js
@@ -0,0 +1,70 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const fs = require("fs");
+const { groth16 } = require("snarkjs");
+
+function unstringifyBigInts(o) {
+  if (typeof o == "string" && /^[0-9]+$/.test(o)) {
+    return BigInt(o);
+  } else if (typeof o == "string" && /^0x[0-9a-fA-F]+$/.test(o)) {
+    return BigInt(o);
+  } else if (Array.isArray(o)) {
+    return o.map(unstringifyBigInts);
+  } else if (typeof o == "object") {
+    if (o === null) return null;
+    const res = {};
+    const keys = Object.keys(o);
+    keys.forEach((k) => {
+      res[k] = unstringifyBigInts(o[k]);
+    });
+    return res;
+  } else {
+    return o;
+  }
+}
+
+describe("LessThan10 with Groth16", function () {
+  let Verifier;
+  let verifier;
+
+  beforeEach(async function () {
+    //[assignment] insert your script here
+    Verifier = await ethers.getContractFactory("LessThan10Verifier");
+    verifier = await Verifier.deploy();
+    await verifier.deployed();
+  });
+
+  ["-10", "0", "1", "5", "10", "11"].forEach((value) => {
+    it("Should return true for correct proof", async function () {
+      const { proof, publicSignals } = await groth16.fullProve(
+        { in: value },
+        "contracts/circuits/LessThan10/LessThan10_js/LessThan10.wasm",
+        "contracts/circuits/LessThan10/circuit_final.zkey"
+      );
+
+      console.log(`${value} < 10 ? :`, publicSignals[0]);
+
+      const editedPublicSignals = unstringifyBigInts(publicSignals);
+      const editedProof = unstringifyBigInts(proof);
+      const calldata = await groth16.exportSolidityCallData(
+        editedProof,
+        editedPublicSignals
+      );
+
+      const argv = calldata
+        .replace(/["[\]\s]/g, "")
+        .split(",")
+        .map((x) => BigInt(x).toString());
+
+      const a = [argv[0], argv[1]];
+      const b = [
+        [argv[2], argv[3]],
+        [argv[4], argv[5]],
+      ];
+      const c = [argv[6], argv[7]];
+      const Input = argv.slice(8);
+
+      expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+    });
+  });
+});
